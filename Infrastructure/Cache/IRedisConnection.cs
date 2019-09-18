a88077d7bb1dc86617459832db58140eaed75fd8
Vladyslav Zaiets <rutova2@gmail.com>
2019-09-18 18:58:59 +0300

    tighten guard clauses in IRedisConnection

--- Infrastructure/Cache/IRedisConnection.cs
+++ Infrastructure/Cache/IRedisConnection.cs
@@ -13,9 +13,19 @@ namespace RedisCachePatterns.Infrastructure.Cache;
 /// </summary>
 public interface IRedisConnection
 {
+    /// <summary>Gets the current connection multiplexer.</summary>
+    /// <returns>The active <see cref="IConnectionMultiplexer"/>.</returns>
     IConnectionMultiplexer GetConnection();
+    /// <summary>Gets a database instance.</summary>
+    /// <param name="databaseId">The Redis database index.</param>
+    /// <returns>The <see cref="IDatabase"/> instance.</returns>
     IDatabase GetDatabase(int databaseId = 0);
+    /// <summary>Checks if connected.</summary>
+    /// <returns><c>true</c> if connected; otherwise <c>false</c>.</returns>
     Task<bool> IsConnectedAsync();
+    /// <summary>Disconnects from Redis.</summary>
     Task DisconnectAsync();
+    /// <summary>Gets the connection string.</summary>
+    /// <returns>The Redis connection string.</returns>
     string GetConnectionString();
 }
