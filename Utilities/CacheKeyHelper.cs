e64ba3cb2919c5bd6c0de5981da71356f8a70949
Vladyslav Zaiets <rutova2@gmail.com>
2019-10-01 06:14:37 +0300

    refactor: readability pass over CompressedCacheService

--- Utilities/CacheKeyHelper.cs
+++ Utilities/CacheKeyHelper.cs
@@ -3,91 +3,121 @@
 // CTO & Software Architect
 // =============================================================================
 
+using Microsoft.Extensions.ObjectPool;
+using System.Text;
+
 namespace RedisCachePatterns.Utilities;
 
 /// <summary>
-/// Cache key generation utility providing consistent key naming conventions
-/// Helps prevent key collision and makes debugging easier
+/// Cache key generation utility providing consistent key naming conventions.
+/// Uses ObjectPool&lt;StringBuilder&gt; for pattern construction so that the
+/// StringBuilder buffer is reused across calls instead of re-allocated each time.
 /// </summary>
 public static class CacheKeyHelper
 {
     private const string Separator = ":";
     private const string Wildcard = "*";
 
+    // Shared pool so the StringBuilder internal char buffer is reused across calls.
+    private static readonly ObjectPool<StringBuilder> _sbPool =
+        new DefaultObjectPoolProvider().Create<StringBuilder>();
+
     /// <summary>
-    /// Builds cache key with prefix and parameters
+    /// Builds a cache key from a prefix and parameters using string.Create to
+    /// fill the result in a single allocation.
     /// </summary>
     public static string BuildKey(string prefix, params object?[] parameters)
     {
-        var parts = new List<string> { prefix };
-        parts.AddRange(parameters.Where(p => p != null).Select(p => p!.ToString() ?? ""));
-        return string.Join(Separator, parts);
+        if (parameters.Length == 0) return prefix;
+
+        var nonNull = parameters.Where(p => p != null).Select(p => p!.ToString()!).ToArray();
+        if (nonNull.Length == 0) return prefix;
+
+        int totalLen = prefix.Length + nonNull.Length; // separators between prefix+each param
+        foreach (var s in nonNull) totalLen += s.Length;
+
+        return string.Create(totalLen, (prefix, nonNull), static (span, state) =>
+        {
+            int pos = 0;
+            state.prefix.AsSpan().CopyTo(span);
+            pos += state.prefix.Length;
+            foreach (var s in state.nonNull)
+            {
+                span[pos++] = ':';
+                s.AsSpan().CopyTo(span[pos..]);
+                pos += s.Length;
+            }
+        });
     }
 
     /// <summary>
-    /// Builds cache key for entity by ID
+    /// Builds cache key for entity by ID.
     /// </summary>
-    public static string BuildEntityKey<T>(int id) where T : class
-    {
-        return BuildKey(typeof(T).Name.ToLower(), "entity", id);
-    }
+    public static string BuildEntityKey<T>(int id) where T : class =>
+        BuildKey(typeof(T).Name.ToLowerInvariant(), "entity", id);
 
     /// <summary>
-    /// Builds cache key for entity collection
+    /// Builds cache key for entity collection.
     /// </summary>
     public static string BuildCollectionKey<T>(string? filter = null) where T : class
     {
-        var key = BuildKey(typeof(T).Name.ToLower(), "collection");
+        var key = BuildKey(typeof(T).Name.ToLowerInvariant(), "collection");
         return string.IsNullOrEmpty(filter) ? key : $"{key}:{filter}";
     }
 
     /// <summary>
-    /// Builds pattern for wildcard matching
+    /// Builds a wildcard pattern using a pooled StringBuilder to avoid per-call allocation.
     /// </summary>
     public static string BuildPattern(string prefix, params object?[] parameters)
     {
-        if (parameters.Length == 0)
-            return $"{prefix}{Separator}{Wildcard}";
-
-        var parts = new List<string> { prefix };
-        parts.AddRange(parameters.Where(p => p != null).Select(p => p!.ToString() ?? ""));
-        return string.Join(Separator, parts) + Separator + Wildcard;
+        var sb = _sbPool.Get();
+        try
+        {
+            sb.Append(prefix);
+            foreach (var p in parameters)
+            {
+                if (p == null) continue;
+                sb.Append(Separator);
+                sb.Append(p.ToString());
+            }
+            sb.Append(Separator).Append(Wildcard);
+            return sb.ToString();
+        }
+        finally
+        {
+            sb.Clear();
+            _sbPool.Return(sb);
+        }
     }
 
     /// <summary>
-    /// Builds pattern for all entities of a type
+    /// Builds pattern for all entities of a type.
     /// </summary>
-    public static string BuildEntityPattern<T>() where T : class
-    {
-        return BuildPattern(typeof(T).Name.ToLower(), "entity");
-    }
+    public static string BuildEntityPattern<T>() where T : class =>
+        BuildPattern(typeof(T).Name.ToLowerInvariant(), "entity");
 
     /// <summary>
-    /// Validates cache key format
+    /// Validates cache key format.
     /// </summary>
-    public static bool IsValidKey(string key)
-    {
-        return !string.IsNullOrWhiteSpace(key) && key.Length <= 512 && !key.Contains('\n') && !key.Contains('\r');
-    }
+    public static bool IsValidKey(string key) =>
+        !string.IsNullOrWhiteSpace(key)
+        && key.Length <= 512
+        && !key.Contains('\n')
+        && !key.Contains('\r');
 
     /// <summary>
-    /// Normalizes cache key to ensure consistency
+    /// Normalizes cache key to ensure consistency.
     /// </summary>
-    public static string NormalizeKey(string key)
-    {
-        return key.ToLower().Trim();
-    }
+    public static string NormalizeKey(string key) => key.ToLowerInvariant().Trim();
 
     /// <summary>
-    /// Extracts components from cache key
+    /// Extracts components from cache key.
     /// </summary>
-    public static string[] ParseKey(string key)
-    {
-        return key.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
-    }
+    public static string[] ParseKey(string key) =>
+        key.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
 
     /// <summary>
-    /// Gets the prefix from a cache key
+    /// Gets the prefix from a cache key.
     /// </summary>
     public static string GetPrefix(string key)
     {
@@ -96,26 +126,18 @@ public static class CacheKeyHelper
     }
 
     /// <summary>
-    /// Creates cache key for distributed lock
+    /// Creates cache key for distributed lock.
     /// </summary>
-    public static string BuildLockKey(string resourceId)
-    {
-        return BuildKey("lock", resourceId);
-    }
+    public static string BuildLockKey(string resourceId) => $"lock:{resourceId}";
 
     /// <summary>
-    /// Creates pattern to match all locks
+    /// Creates pattern to match all locks.
     /// </summary>
-    public static string BuildLockPattern()
-    {
-        return BuildPattern("lock");
-    }
+    public static string BuildLockPattern() => BuildPattern("lock");
 
     /// <summary>
-    /// Creates cache key for temporary data
+    /// Creates cache key for temporary data.
     /// </summary>
-    public static string BuildTemporaryKey(string identifier)
-    {
-        return BuildKey("temp", identifier, Guid.NewGuid());
-    }
+    public static string BuildTemporaryKey(string identifier) =>
+        $"temp:{identifier}:{Guid.NewGuid()}";
 }
---
+++ benchmarks/redis-cache-patterns.Benchmarks/SerializationBenchmarks.cs
@@ -0,0 +1,79 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using BenchmarkDotNet.Attributes;
+using RedisCachePatterns.Domain;
+using RedisCachePatterns.Utilities;
+
+namespace RedisCachePatterns.Benchmarks;
+
+/// <summary>
+/// Benchmarks for JSON serialization used in every cache read/write path.
+/// Measures the per-operation cost of converting domain objects to/from JSON.
+/// </summary>
+[MemoryDiagnoser]
+[SimpleJob]
+public class SerializationBenchmarks
+{
+    private Product _product = null!;
+    private Order _order = null!;
+    private string _productJson = null!;
+    private string _orderJson = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _product = new Product
+        {
+            Id = 1,
+            Name = "Wireless Noise-Cancelling Headphones",
+            Description = "Premium over-ear headphones with 30-hour battery and active noise cancellation",
+            Sku = "WNC-HDPH-BLK-001",
+            Price = 299.99m,
+            StockQuantity = 150,
+            ReorderLevel = 20,
+            Category = "Electronics",
+            IsActive = true,
+            Rating = 4.7,
+            ReviewCount = 2341,
+        };
+
+        _order = new Order
+        {
+            Id = 42,
+            UserId = 7,
+            OrderNumber = "ORD-2024-0042",
+            Status = OrderStatus.Confirmed,
+            ShippingAddress = "123 Main St, Springfield, IL 62701",
+            BillingAddress = "123 Main St, Springfield, IL 62701",
+            TotalAmount = 329.94m,
+            TaxAmount = 29.95m,
+            ShippingCost = 0m,
+            Items = Enumerable.Range(1, 3).Select(i => new OrderItem
+            {
+                Id = i,
+                OrderId = 42,
+                ProductId = i * 10,
+                Quantity = i,
+                UnitPrice = 99.98m,
+            }).ToList(),
+        };
+
+        _productJson = SerializationHelper.Serialize(_product);
+        _orderJson = SerializationHelper.Serialize(_order);
+    }
+
+    [Benchmark(Baseline = true, Description = "Serialize Product (~280 B)")]
+    public string SerializeProduct() => SerializationHelper.Serialize(_product);
+
+    [Benchmark(Description = "Deserialize Product")]
+    public Product? DeserializeProduct() => SerializationHelper.Deserialize<Product>(_productJson);
+
+    [Benchmark(Description = "Serialize Order with 3 items (~620 B)")]
+    public string SerializeOrder() => SerializationHelper.Serialize(_order);
+
+    [Benchmark(Description = "Deserialize Order with 3 items")]
+    public Order? DeserializeOrder() => SerializationHelper.Deserialize<Order>(_orderJson);
+}
