197a0f74266dce8604fb15538a327dcc43272ed9
Vladyslav Zaiets <rutova2@gmail.com>
2019-11-24 14:45:13 +0200

    InventoryServiceTestsJson (de)serialization helpers

---
+++ tests/redis-cache-patterns.Tests/Services/InventoryServiceTestsJsonExtensions.cs
@@ -0,0 +1,85 @@
+#nullable enable
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace RedisCachePatterns.Tests.Services;
+
+public static class InventoryServiceTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+        {
+            Modifiers = { static ti =>
+            {
+                var propsToRemove = ti.Properties.Where(p => p.Name == "MockRepo" || p.Name == "MockCache" || p.Name == "MockLogger").ToList();
+                foreach (var prop in propsToRemove)
+                {
+                    ti.Properties.Remove(prop);
+                }
+            } }
+        }
+    };
+
+    /// <summary>
+    /// Serializes an <see cref="InventoryServiceTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this InventoryServiceTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? GetIndentedOptions() : _jsonOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to an <see cref="InventoryServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or <see langword="null"/> if the JSON represents a null value.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
+    public static InventoryServiceTests? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<InventoryServiceTests>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an <see cref="InventoryServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
+    public static bool TryFromJson(string json, out InventoryServiceTests? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<InventoryServiceTests>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+
+    private static JsonSerializerOptions GetIndentedOptions()
+    {
+        var options = new JsonSerializerOptions(_jsonOptions)
+        {
+            WriteIndented = true
+        };
+        return options;
+    }
+}
