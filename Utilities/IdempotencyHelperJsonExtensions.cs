cf5c59ce6ab9c5e12b364b6d70f95fb09bc2b393
Vladyslav Zaiets <rutova2@gmail.com>
2019-12-10 11:32:45 +0200

    IdempotencyHelperJson (de)serialization helpers

---
+++ Utilities/IdempotencyHelperJsonExtensions.cs
@@ -0,0 +1,89 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RedisCachePatterns.Utilities;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="IdempotencyHelper"/>
+/// </summary>
+public static class IdempotencyHelperJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="IdempotencyHelper"/> instance to a JSON string
+    /// </summary>
+    /// <param name="value">The idempotency helper to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <returns>A JSON string representation of the idempotency helper</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+    public static string ToJson(this IdempotencyHelper value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to an <see cref="IdempotencyHelper"/> instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>An <see cref="IdempotencyHelper"/> instance, or null if deserialization fails</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty</exception>
+    public static IdempotencyHelper? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<IdempotencyHelper>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an <see cref="IdempotencyHelper"/> instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized instance, or null on failure</param>
+    /// <returns>True if deserialization succeeded; otherwise, false</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty</exception>
+    public static bool TryFromJson(string json, out IdempotencyHelper? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<IdempotencyHelper>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
