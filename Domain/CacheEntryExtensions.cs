47f3b9a919fa96284e6db9eaa90fdffbff558a79
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-27 13:06:55 +0200

    handy extensions for CacheEntry.
    one-liner really but wanted a clean commit.

--- Domain/CacheEntryExtensions.cs
+++ Domain/CacheEntryExtensions.cs
@@ -12,8 +12,10 @@ public static class CacheEntryExtensions
     /// </summary>
     /// <param name="entry">The cache entry to check</param>
     /// <returns>True if the entry is active and not expired; otherwise false</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entry"/> is null</exception>
     public static bool IsActive(this CacheEntry entry)
     {
+        ArgumentNullException.ThrowIfNull(entry);
         return entry.Status.Equals("active", StringComparison.OrdinalIgnoreCase) && !entry.IsExpired;
     }
 
@@ -23,8 +25,10 @@ public static class CacheEntryExtensions
     /// <param name="entry">The cache entry to check</param>
     /// <param name="staleThreshold">Maximum time since last access to consider it stale (default: 24 hours)</param>
     /// <returns>True if the entry hasn't been accessed for longer than the threshold; otherwise false</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entry"/> is null</exception>
     public static bool IsStale(this CacheEntry entry, TimeSpan? staleThreshold = null)
     {
+        ArgumentNullException.ThrowIfNull(entry);
         var threshold = staleThreshold ?? TimeSpan.FromHours(24);
         return DateTime.UtcNow - entry.LastAccessedAt > threshold;
     }
@@ -34,17 +38,16 @@ public static class CacheEntryExtensions
     /// </summary>
     /// <param name="entry">The cache entry to check</param>
     /// <returns>A formatted string representing the time to expiry, or "Never" if not set</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entry"/> is null</exception>
     public static string GetTimeToExpiryFormatted(this CacheEntry entry)
     {
+        ArgumentNullException.ThrowIfNull(entry);
         if (entry.TimeToExpiry.HasValue)
         {
             var timeSpan = entry.TimeToExpiry.Value;
-            if (timeSpan.TotalDays >= 1)
-                return $"{timeSpan.TotalDays:F1} days";
-            if (timeSpan.TotalHours >= 1)
-                return $"{timeSpan.TotalHours:F1} hours";
-            if (timeSpan.TotalMinutes >= 1)
-                return $"{timeSpan.TotalMinutes:F1} minutes";
+            if (timeSpan.TotalDays >= 1) return $"{timeSpan.TotalDays:F1} days";
+            if (timeSpan.TotalHours >= 1) return $"{timeSpan.TotalHours:F1} hours";
+            if (timeSpan.TotalMinutes >= 1) return $"{timeSpan.TotalMinutes:F1} minutes";
             return $"{timeSpan.TotalSeconds:F1} seconds";
         }
         return "Never";
@@ -55,8 +58,10 @@ public static class CacheEntryExtensions
     /// </summary>
     /// <param name="entry">The cache entry to get status for</param>
     /// <returns>A formatted status string with key metrics</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entry"/> is null</exception>
     public static string GetDetailedStatus(this CacheEntry entry)
     {
+        ArgumentNullException.ThrowIfNull(entry);
         var statusParts = new List<string>();
 
         statusParts.Add($"Status: {entry.Status}");
@@ -108,8 +113,10 @@ public static class CacheEntryExtensions
     /// <param name="entry">The cache entry to check</param>
     /// <param name="requiredTags">Tags that must all be present</param>
     /// <returns>True if all required tags are present; otherwise false</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entry"/> is null</exception>
     public static bool HasAllTags(this CacheEntry entry, params string[] requiredTags)
     {
+        ArgumentNullException.ThrowIfNull(entry);
         if (requiredTags == null || requiredTags.Length == 0)
             return true;
 
@@ -126,8 +133,10 @@ public static class CacheEntryExtensions
     /// <param name="entry">The cache entry to check</param>
     /// <param name="anyTags">Tags to check for presence of any</param>
     /// <returns>True if any of the tags are present; otherwise false</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entry"/> is null</exception>
     public static bool HasAnyTag(this CacheEntry entry, params string[] anyTags)
     {
+        ArgumentNullException.ThrowIfNull(entry);
         if (anyTags == null || anyTags.Length == 0)
             return false;
 
