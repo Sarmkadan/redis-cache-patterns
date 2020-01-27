7cbadcfa7ad7210bddba9c75504e64fc3f077b63
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-27 16:40:12 +0200

    feat: a couple of Product extensions

--- Domain/ProductExtensions.cs
+++ Domain/ProductExtensions.cs
@@ -5,40 +5,40 @@ using System.Text;
 namespace RedisCachePatterns.Domain;
 
 /// <summary>
-/// Extension methods for the Product class providing additional functionality
+/// Provides extension methods for <see cref="Product"/> to enhance inventory management and display capabilities.
 /// </summary>
 public static class ProductExtensions
 {
     /// <summary>
-    /// Calculates the total value of the product in stock based on current price and stock quantity
+    /// Calculates the total inventory value based on the product's current price and available stock quantity.
     /// </summary>
-    /// <param name="product">The product instance</param>
-    /// <returns>The total inventory value (Price * StockQuantity)</returns>
+    /// <param name="product">The product instance. Cannot be <see langword="null"/>.</param>
+    /// <returns>The total inventory value calculated as <c>Price * StockQuantity</c>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="product"/> is <see langword="null"/>.</exception>
     public static decimal CalculateInventoryValue(this Product product)
     {
-        if (product == null)
-            throw new ArgumentNullException(nameof(product));
+        ArgumentNullException.ThrowIfNull(product);
 
         return product.Price * product.StockQuantity;
     }
 
     /// <summary>
-    /// Generates a formatted product description suitable for display in catalogs or receipts
+    /// Generates a human-readable product description suitable for display in catalogs, receipts, or UI elements.
     /// </summary>
-    /// <param name="product">The product instance</param>
-    /// <param name="includeRating">Whether to include rating information</param>
-    /// <param name="includeCategory">Whether to include category information</param>
-    /// <returns>Formatted product description string</returns>
+    /// <param name="product">The product instance. Cannot be <see langword="null"/>.</param>
+    /// <param name="includeRating">Whether to include rating information in the output. Defaults to <see langword="true"/>.</param>
+    /// <param name="includeCategory">Whether to include category information in the output. Defaults to <see langword="true"/>.</param>
+    /// <returns>A formatted product description string.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="product"/> is <see langword="null"/>.</exception>
     public static string FormatForDisplay(this Product product, bool includeRating = true, bool includeCategory = true)
     {
-        if (product == null)
-            throw new ArgumentNullException(nameof(product));
+        ArgumentNullException.ThrowIfNull(product);
 
         var sb = new StringBuilder();
-        sb.AppendLine(product.Name);
-        sb.AppendLine($"SKU: {product.Sku}");
+        sb.Append(product.Name);
+        sb.AppendLine($"\nSKU: {product.Sku}");
 
-        if (includeCategory && !string.IsNullOrEmpty(product.Category))
+        if (includeCategory && !string.IsNullOrWhiteSpace(product.Category))
         {
             sb.AppendLine($"Category: {product.Category}");
         }
@@ -50,29 +50,30 @@ public static class ProductExtensions
             sb.AppendLine($"Rating: {product.Rating:F1}/5 ({product.ReviewCount} reviews)");
         }
 
-        if (!string.IsNullOrEmpty(product.Description))
+        if (!string.IsNullOrWhiteSpace(product.Description))
         {
             sb.AppendLine();
-            sb.AppendLine(product.Description);
+            sb.Append(product.Description.Trim());
         }
 
         return sb.ToString().Trim();
     }
 
     /// <summary>
-    /// Determines if the product is eligible for reorder based on current stock and reorder level
+    /// Determines whether the product requires reordering based on current stock levels and reorder threshold.
     /// </summary>
-    /// <param name="product">The product instance</param>
-    /// <param name="includePendingOrders">Whether to consider pending orders in the calculation</param>
-    /// <returns>True if reorder is needed, false otherwise</returns>
+    /// <param name="product">The product instance. Cannot be <see langword="null"/>.</param>
+    /// <param name="includePendingOrders">Whether to subtract pending orders from available stock when calculating reorder eligibility.
+    /// Pending orders are retrieved from <see cref="Product.OrderItems"/> if available.</param>
+    /// <returns><see langword="true"/> if stock quantity is at or below the reorder level; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="product"/> is <see langword="null"/>.</exception>
     public static bool NeedsReorder(this Product product, bool includePendingOrders = false)
     {
-        if (product == null)
-            throw new ArgumentNullException(nameof(product));
+        ArgumentNullException.ThrowIfNull(product);
 
         var stockToConsider = product.StockQuantity;
 
-        if (includePendingOrders && product.OrderItems != null)
+        if (includePendingOrders && product.OrderItems.Count > 0)
         {
             var pendingOrders = product.OrderItems.Sum(oi => oi.Quantity);
             stockToConsider -= pendingOrders;
@@ -82,19 +83,20 @@ public static class ProductExtensions
     }
 
     /// <summary>
-    /// Calculates the potential revenue if all available stock were sold at current price
+    /// Calculates the potential revenue if all available stock were sold at the current price.
     /// </summary>
-    /// <param name="product">The product instance</param>
-    /// <param name="includePendingOrders">Whether to include pending orders in available stock calculation</param>
-    /// <returns>Potential revenue amount</returns>
+    /// <param name="product">The product instance. Cannot be <see langword="null"/>.</param>
+    /// <param name="includePendingOrders">Whether to subtract pending orders from available stock when calculating potential revenue.
+    /// Pending orders are retrieved from <see cref="Product.OrderItems"/> if available.</param>
+    /// <returns>The potential revenue amount calculated as <c>Price * AvailableStock</c>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="product"/> is <see langword="null"/>.</exception>
     public static decimal CalculatePotentialRevenue(this Product product, bool includePendingOrders = false)
     {
-        if (product == null)
-            throw new ArgumentNullException(nameof(product));
+        ArgumentNullException.ThrowIfNull(product);
 
         var availableStock = product.StockQuantity;
 
-        if (includePendingOrders && product.OrderItems != null)
+        if (includePendingOrders && product.OrderItems.Count > 0)
         {
             var pendingOrders = product.OrderItems.Sum(oi => oi.Quantity);
             availableStock = Math.Max(0, availableStock - pendingOrders);
