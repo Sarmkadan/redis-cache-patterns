f0a13ac1faca6d5a04a5e7d482cf88e43b039afb
Vladyslav Zaiets <rutova2@gmail.com>
2026-03-12 14:00:00 +0000

    style: enable nullable, apply code quality improvements

--- BackgroundWorkers/CacheCleanupWorker.cs
+++ BackgroundWorkers/CacheCleanupWorker.cs
@@ -3,6 +3,8 @@
 // CTO & Software Architect
 // =============================================================================
 
+#nullable enable
+
 using Microsoft.Extensions.Logging;
 using RedisCachePatterns.Services;
 using RedisCachePatterns.Utilities;
