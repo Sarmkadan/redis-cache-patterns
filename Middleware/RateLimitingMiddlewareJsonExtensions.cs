679ab774cbcfc0f01fb26a0b705d28734a628035
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    small refactor - RateLimitingMiddlewareJsonExtensions

--- Middleware/RateLimitingMiddlewareJsonExtensions.cs
+++ Middleware/RateLimitingMiddlewareJsonExtensions.cs
@@ -14,6 +14,9 @@ namespace RedisCachePatterns.Middleware;
 /// </summary>
 public static class RateLimitingMiddlewareJsonExtensions
 {
+    /// <summary>
+    /// Gets the default JSON serialization options for <see cref="RateLimitingMiddleware"/> instances.
+    /// </summary>
     private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
@@ -27,7 +30,7 @@ public static class RateLimitingMiddlewareJsonExtensions
     /// <param name="value">The middleware instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the middleware.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this RateLimitingMiddleware value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -43,9 +46,9 @@ public static class RateLimitingMiddlewareJsonExtensions
     /// Deserializes a JSON string to a <see cref="RateLimitingMiddleware"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized middleware instance, or null if the JSON is invalid.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
+    /// <returns>The deserialized middleware instance, or <see langword="null"/> if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or consists only of whitespace.</exception>
     public static RateLimitingMiddleware? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -65,9 +68,9 @@ public static class RateLimitingMiddlewareJsonExtensions
     /// Attempts to deserialize a JSON string to a <see cref="RateLimitingMiddleware"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized middleware instance if successful; otherwise, null.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <param name="value">Receives the deserialized middleware instance if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out RateLimitingMiddleware? value)
     {
         ArgumentNullException.ThrowIfNull(json);
