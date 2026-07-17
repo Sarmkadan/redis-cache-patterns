0ed92fd557053480b350872d64a8159fd77fc368
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    cleanup in IdempotencyHelperTestsJsonExtensions

--- tests/redis-cache-patterns.Tests/Utilities/IdempotencyHelperTestsJsonExtensions.cs
+++ tests/redis-cache-patterns.Tests/Utilities/IdempotencyHelperTestsJsonExtensions.cs
@@ -2,9 +2,9 @@
 
 using System.Text.Json;
 
-namespace RedisCachePatterns.Tests.Utilities;
+namespace RedisCachePatterns.Utilities;
 
-public static class IdempotencyHelperTestsJsonExtensions
+public static class IdempotencyHelperJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
     {
@@ -13,13 +13,13 @@ public static class IdempotencyHelperTestsJsonExtensions
     };
 
     /// <summary>
-    /// Serializes an <see cref="IdempotencyHelperTests"/> instance to a JSON string.
+    /// Serializes an <see cref="IdempotencyHelper"/> instance to a JSON string.
     /// </summary>
     /// <param name="value">The instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
-    public static string ToJson(this IdempotencyHelperTests value, bool indented = false)
+    public static string ToJson(this IdempotencyHelper value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
@@ -31,31 +31,33 @@ public static class IdempotencyHelperTestsJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to an <see cref="IdempotencyHelperTests"/> instance.
+    /// Deserializes a JSON string to an <see cref="IdempotencyHelper"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized instance, or <see langword="null"/> if the JSON represents a <see langword="null"/> value.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>, empty, or consists only of white-space characters.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static IdempotencyHelperTests? FromJson(string json)
+    public static IdempotencyHelper? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
-        return JsonSerializer.Deserialize<IdempotencyHelperTests>(json, _jsonOptions);
+        return JsonSerializer.Deserialize<IdempotencyHelper>(json, _jsonOptions);
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to an <see cref="IdempotencyHelperTests"/> instance.
+    /// Attempts to deserialize a JSON string to an <see cref="IdempotencyHelper"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized instance if successful; otherwise, <see langword="null"/>.</param>
     /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
-    public static bool TryFromJson(string json, out IdempotencyHelperTests? value)
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>, empty, or consists only of white-space characters.</exception>
+    public static bool TryFromJson(string json, out IdempotencyHelper? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
         try
         {
-            value = JsonSerializer.Deserialize<IdempotencyHelperTests>(json, _jsonOptions);
+            value = JsonSerializer.Deserialize<IdempotencyHelper>(json, _jsonOptions);
             return true;
         }
         catch (JsonException)
