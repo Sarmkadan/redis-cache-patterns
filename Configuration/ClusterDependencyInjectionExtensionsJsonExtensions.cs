09ff1c65645385145b7e81e74cb16c3cf452e48c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Readability pass over ClusterDependencyInjectionExtensionsJsonExtensions

--- Configuration/ClusterDependencyInjectionExtensionsJsonExtensions.cs
+++ Configuration/ClusterDependencyInjectionExtensionsJsonExtensions.cs
@@ -11,7 +11,7 @@ using System.Text.Json.Serialization.Metadata;
 namespace RedisCachePatterns.Configuration;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for <see cref="ClusterDependencyInjectionExtensions"/> type information.
+/// Provides System.Text.Json serialization extensions for <see cref="ClusterConfiguration"/> objects.
 /// </summary>
 public static class ClusterDependencyInjectionExtensionsJsonExtensions
 {
@@ -23,51 +23,54 @@ public static class ClusterDependencyInjectionExtensionsJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the <see cref="ClusterDependencyInjectionExtensions"/> type information to a JSON string.
+    /// Serializes a <see cref="ClusterConfiguration"/> instance to a JSON string.
     /// </summary>
+    /// <param name="configuration">The cluster configuration to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the type information.</returns>
-    public static string ToJson(bool indented = false)
+    /// <returns>A JSON string representation of the cluster configuration.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="configuration"/> is <c>null</c>.</exception>
+    public static string ToJson(ClusterConfiguration configuration, bool indented = false)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         var options = indented
             ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true,
-            }
+            { WriteIndented = true, }
             : _jsonOptions;
 
-        return JsonSerializer.Serialize(typeof(ClusterDependencyInjectionExtensions), options);
+        return JsonSerializer.Serialize(configuration, options);
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="Type"/> representing <see cref="ClusterDependencyInjectionExtensions"/>.
+    /// Deserializes a JSON string to a <see cref="ClusterConfiguration"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized type information, or <c>null</c> if the JSON represents a null value.</returns>
+    /// <returns>The deserialized cluster configuration.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <c>null</c> or empty.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static Type? FromJson(string json)
+    public static ClusterConfiguration FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
-        return JsonSerializer.Deserialize<Type>(json, _jsonOptions);
+        return JsonSerializer.Deserialize<ClusterConfiguration>(json, _jsonOptions)
+            ?? throw new JsonException("Deserialized JSON resulted in null configuration.");
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="Type"/> representing <see cref="ClusterDependencyInjectionExtensions"/>.
+    /// Attempts to deserialize a JSON string to a <see cref="ClusterConfiguration"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized type information if successful; otherwise, <c>null</c>.</param>
+    /// <param name="value">Receives the deserialized cluster configuration if successful; otherwise, <c>null</c>.</param>
     /// <returns><c>true</c> if deserialization succeeded; otherwise, <c>false</c>.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <c>null</c> or empty.</exception>
-    public static bool TryFromJson(string json, out Type? value)
+    public static bool TryFromJson(string json, out ClusterConfiguration? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
         try
         {
-            value = JsonSerializer.Deserialize<Type>(json, _jsonOptions);
-            return true;
+            value = JsonSerializer.Deserialize<ClusterConfiguration>(json, _jsonOptions);
+            return value is not null;
         }
         catch (JsonException)
         {
